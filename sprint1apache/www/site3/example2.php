88944c9ed7671f7caba4f53891c66e9314f2f4ab
carlos <caguiarl@fpcoruna.afundacion.org>
2022-09-29 10:41:49 +0200

    Ejercicio 2 example2.php

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>Página de bienvenidas</h1>
+<?php
+function dar_bienvenida($nombre) {
+echo "!Bienvenido/a, " . $nombre . "!";
+}
+dar_bienvenida("Tom Hanks");
+?>
+</body>
+</html>
