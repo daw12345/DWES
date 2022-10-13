9813423c4172773be79f1ece9352f31058fcb179
carlos <caguiarl@fpcoruna.afundacion.org>
2022-10-13 13:05:06 +0200

    Actualización del ejercicio 4

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -31,7 +31,7 @@ mysqli_close($db);
  <p>Deja un nuevo comentario:</p>
 <form action="/comment.php" method="post">
 <textarea rows="4" cols="50" name="new_comment"></textarea><br>
-<input type="hidden" name="cancion_id" value="<?php echo $cancion_id; ?>">
+<input type="hidden" name="pelicula_id" value="<?php echo $pelicula_id; ?>">
 <input type="submit" value="Comentar">
 </form>
 </body>
