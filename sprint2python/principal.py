2facafa67d6818b918ea298b2bd3e735be3764a3
Carlos <caguiarl@fpcoruna.afundacion.org>
2022-10-17 13:36:49 +0200

    Ejericio 5, actualización del archivo principal

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,7 +1,23 @@
+from factorial import func_factorial
 from factorial2 import func_factorial2
 
-num = int(input("Introduce un número: "))
-if num < 0:
-    print("El número es negativo, introduce uno válido.")
+
+print("Cálculo del factorial de un número")
+print("a) Calcular con recursividad")
+print("b) Calcular sin recursividad")
+opcion = input("Introduce una opción: ")
+
+if opcion.lower() == "a":
+    num = int(input("Introduzca un número positivo: "))
+    if num < 0:
+        print("Ese número es negativo, no es válido.")
+    else:
+        print("El factorial de ",num," es ",func_factorial(num))
+elif opcion.lower() == "b":
+    num = int(input("Introduzca un número positivo: "))
+    if num < 0:
+        print("Ese número es negativo, no es válido.")
+    else:
+        print("El factorial de ",num," es ",func_factorial2(num))
 else:
-    print("El factorial de ",num," es ",func_factorial2(num))
+    print("Opción no válida")
