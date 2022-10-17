0c9592079b3e5e15edaab5fb3e18fc1f10d0c804
Carlos <caguiarl@fpcoruna.afundacion.org>
2022-10-17 13:16:50 +0200

    Ejercicio 2, actualización del archivo concurso.py (ampliado)

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,14 +1,63 @@
+# Ponemos la puntuación a 0
+score=0
+
 #mostramos por pantalla la pregunta junto con las opciones posibles
 print ("Cuántos cilindros tiene un motor v8?")
 print ("a)7")
 print ("b)9")
 print ("c)Ninguna de las anteriores")
 
+print(" ")
+
 # Solicitamos la respsuesta por teclado
 respuesta = input('Introduzca su respuesta: ')
 
-#Establecemos la condición para que en función de la respuesta, se muestre un resultado u otro
+
+
+#Establecemos la condición para que en función de la respuesta, se calcule la puntuación
 if respuesta == "Ninguna de las anteriores" or respuesta=="ninguna de las anteriores" or respuesta=="c":
-    print ("Respuesta correcta")
+    score = score + 10
 else:
-    print("Respuesta incorrecta")
+    score = score - 5
+
+print(" ")
+
+#mostramos por pantalla la pregunta siguiente
+print ("Cuál es la capital de Zambia?")
+print ("a)Lusaka")
+print ("b)Lagos")
+print ("c)Windhoek")
+
+# Solicitamos la respsuesta por teclado
+respuesta = input('Introduzca su respuesta: ')
+
+#de nuevo calculamos la puntuación 
+if respuesta == "Lusaka" or  respuesta == "a":
+    score = score + 10
+else:
+    score = score - 5
+
+
+#mostramos por pantalla la pregunta siguiente
+print ("Cuál es la edad de Johnny Depp")
+print ("a)45")
+print ("b)49")
+print ("c)59")
+
+print(" ")
+
+# Solicitamos la respsuesta por teclado
+respuesta = input('Introduzca su respuesta: ')
+
+#de nuevo calculamos la puntuación 
+if respuesta == "59" or respuesta =="c":
+    score = score + 10
+else:
+    score = score - 5
+
+
+print(" ")
+
+#mostramos la puntuación final
+print("Su puntuación final es: ",score)
+
