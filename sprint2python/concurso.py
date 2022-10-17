d8ffc517cce193e07a3dad5693455fe6d03edc63
Carlos <caguiarl@fpcoruna.afundacion.org>
2022-10-17 12:55:11 +0200

    Ejercicio 1 con la carpeta sprint2python

---
+++ sprint2python/concurso.py
@@ -0,0 +1,14 @@
+#mostramos por pantalla la pregunta junto con las opciones posibles
+print ("Cuántos cilindros tiene un motor v8?")
+print ("a)7")
+print ("b)9")
+print ("c)Ninguna de las anteriores")
+
+# Solicitamos la respsuesta por teclado
+respuesta = input('Introduzca su respuesta: ')
+
+#Establecemos la condición para que en función de la respuesta, se muestre un resultado u otro
+if respuesta == "Ninguna de las anteriores" or respuesta=="ninguna de las anteriores" or respuesta=="c":
+    print ("Respuesta correcta")
+else:
+    print("Respuesta incorrecta")
