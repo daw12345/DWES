43801e5dd246a76eae4bc79dee26b585828eea30
carlos <caguiarl@fpcoruna.afundacion.org>
2022-11-16 14:08:47 +0100

    Atajo get_object_or_404()

--- tutorial_django/mysite/polls/views.py
+++ tutorial_django/mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http from Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 
@@ -8,10 +8,7 @@ from .models import Question
 
 
 def detail(request, question_id):
-	try:
-		question =  Question.objects.get(pk=question_id)
-	except Question.DoesNoExist:
-		raise Http404("Question does not exist")
+	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
