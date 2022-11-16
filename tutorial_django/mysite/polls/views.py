f78f32f434a8b07387497b43040e7a7c33ee4f20
carlos <caguiarl@fpcoruna.afundacion.org>
2022-11-16 14:02:35 +0100

    Levantar un error 404

--- tutorial_django/mysite/polls/views.py
+++ tutorial_django/mysite/polls/views.py
@@ -1,3 +1,4 @@
+from django.http from Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -7,7 +8,11 @@ from .models import Question
 
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	try:
+		question =  Question.objects.get(pk=question_id)
+	except Question.DoesNoExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
